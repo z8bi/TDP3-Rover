6f5d084f53f565937e48662d580f562aff2a7b58
z8bi <azarzabiallah@gmail.com>
2026-02-04 14:07:46 +0000

    added TCRT digital in to pinassignments header file

--- include/pinassignments.hpp
+++ include/pinassignments.hpp
@@ -10,5 +10,8 @@ DigitalOut INI1_A(D2); //PTD4
 DigitalOut INI2_A(D9); //PTD5
 DigitalOut INI3_B(D8); //PTA13
 DigitalOut INI4_B(D10); //PTD0
+DigitalIn Left_TCRT(D0); //PTA2
+DigitalIn Right_TCRT(D1); //PTA1
+DigitalIn Centre_TCRT(D4); //PTA4
 
 #endif // PINASSIGNMENTS_HPP
